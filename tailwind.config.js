75d23628d586c12056a853b52e4ba0e3f02f0e19
mcmpbll783 <mcampbell327@yahoo.com.ar>
2020-10-31 17:08:40 -0300

    Push inicial del proyecto completo

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    purge: [
+      'src/**/*.js',
+      'src/**/*.jsx',
+      'src/**/*.ts',
+      'src/**/*.tsx',
+      'public/**/*.html',
+    ],
+    theme: {
+        fontFamily: {
+          logotipo: [ 'Open Sans', 'sans-serif' ],
+          headers: [ 'Roboto', 'sans-serif' ],
+          body: [ 'Lato', 'sans-serif' ]
+        },
+        extend: {
+          spacing: {
+            '14': '3.5rem'
+          },
+          colors: {
+            primary: {
+              '100': '#EBF4FF',
+              '200': '#B5D1F2',
+              '300': '#95BCE6',
+              '400': '#629DD9',
+              '500': '#3182CE',
+              '600': '#356599',
+              '700': '#2A4365',
+              '800': '#273747',
+              '900': '#1A2533',
+            }
+          }
+        },
+      },
+      variants: {},
+      plugins: [],
+    }    
